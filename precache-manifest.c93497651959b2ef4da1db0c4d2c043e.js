83366d2afa7c5c904e0e875d34e7387db734bd74
praveend <praveend2622@gmail.com>
2021-03-17 20:25:33 +0530

    Updates

---
+++ precache-manifest.c93497651959b2ef4da1db0c4d2c043e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c4d5b7ebe32618e7656a3e24bcd2101",
+    "url": "/hulu/index.html"
+  },
+  {
+    "revision": "dd911cb3cd318d9593da",
+    "url": "/hulu/static/css/main.93c68cb2.chunk.css"
+  },
+  {
+    "revision": "ffe1110889cc8c927d97",
+    "url": "/hulu/static/js/2.bb7c0a09.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hulu/static/js/2.bb7c0a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd911cb3cd318d9593da",
+    "url": "/hulu/static/js/main.067c56b7.chunk.js"
+  },
+  {
+    "revision": "b09dda2c611f8cf73e2b",
+    "url": "/hulu/static/js/runtime-main.af96a8ac.js"
+  }
+]);
